5ac1744722a5add5607f58953e53c2af78c4639f
Steven Kaifeng Ren <stevenfrog@outlook.com>
2024-12-11 18:20:37 +1030

    add time diff

--- src/components/finish-list/index.jsx
+++ src/components/finish-list/index.jsx
@@ -1,31 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import './index.css'
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import KeyboardReturnOutlinedIcon from '@mui/icons-material/KeyboardReturnOutlined';
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import KeyboardReturnOutlinedIcon from "@mui/icons-material/KeyboardReturnOutlined";
+import { useNavigate } from "react-router-dom";
 
-export default function FinshList({finishedItems,revertComplete,deleteEvent}) {
+export default function FinshList({
+  finishedItems,
+  revertComplete,
+  deleteEvent,
+}) {
+  const navigate = useNavigate();
 
-    finishedItems = finishedItems || [];
+  finishedItems = finishedItems || [];
 
-    return (
-    <div className='finsh-list-container'>
-        <ul className='list-group'>
-        {
-            finishedItems.map((event,index)=>{
-                return (<li key={index} className='list-group-item d-flex flex-wrap justify-content-between align-items-center'>
-                  <b className=' col-sm-1  col-md-1' >event Name:</b> <p className='col-sm-1 col-md-2'>{event.toDo} </p> 
-                <b className='col-sm-1  col-md-1'>   finish Date: </b > <p className='col-sm-1 col-md-2'>{event.finishDate}</p>
-                
-                <button className='revert-btn col-sm-1  col-md-1'  aria-label="revert evenet"  onClick={()=>revertComplete(event.id)}>
-                <KeyboardReturnOutlinedIcon/>
-                </button>
-                <button className='delete-btn col-sm-1  col-md-1'  aria-label="delete evenet" onClick={() => deleteEvent(event.id)}>
+  return (
+    <div className="finsh-list-container">
+      <ul className="list-group">
+        {finishedItems.map((event, index) => {
+          return (
+            <li
+              key={index}
+              className="list-group-item d-flex flex-wrap justify-content-between align-items-center"
+            >
+              <b className=" col-sm-1  col-md-1">event Name:</b>{" "}
+              <p className="col-sm-1 col-md-2">{event.toDo} </p>
+              <b className="col-sm-1  col-md-1"> finish Date: </b>{" "}
+              <p className="col-sm-1 col-md-2">{event.finishDate}</p>
+              <button
+                className="revert-btn col-sm-1  col-md-1"
+                aria-label="revert event"
+                onClick={() => revertComplete(event.id)}
+              >
+                <KeyboardReturnOutlinedIcon />
+              </button>
+              <button
+                className="delete-btn col-sm-1  col-md-1"
+                aria-label="delete event"
+                onClick={() => deleteEvent(event.id)}
+              >
                 <DeleteForeverIcon />
-                </button>
-                </li>) 
-            })
-        }
-        </ul>
+              </button>
+              <button
+                className="detail-btn col-sm-1  col-md-1"
+                aria-label="delete event"
+                onClick={() => {
+                  navigate(`/todo-detail/${event.id}`);
+                }}
+              >
+                Detail
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
---
+++ src/components/item-detail/index.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { data, useParams } from "react-router-dom";
+import formatDateToIso from "../format-method/formatDateToIso";
+import GetTimeDiff from "./get-time-diff";
+import Warning from "./warning";
+import './index.css'
+
+export default function ItemDetail(props) {
+  const { id } = useParams();
+  const [postDetail, setPostDetail] = useState(null);
+  const [timeRemain, setTimeRemain] = useState(null);
+  const [haveTimeDiff, sethaveTimeDiff] = useState(false);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("eventsList"));
+    const item = data.find((item) => item.id === id); //get particular item from event list
+    console.log(item);
+    setPostDetail(item);
+    if (!item.Complete) {
+      sethaveTimeDiff(true);
+    }
+  }, [id]);
+
+  if (!postDetail) {
+    return <div>404 Not found</div>;
+  }
+
+
+
+  return (
+    <div className="task-detail-container">
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Time Created: </th>
+            <td> {postDetail.createTime}</td>
+          </tr>
+          <tr>
+            <th scope="row">Deadline: </th>
+            <td>{formatDateToIso(postDetail.dueDate)}</td>
+          </tr>
+          <tr>
+            <th scope="row">Subtasks</th>
+            <td>
+              <ul className="list-group">
+                {postDetail.Subtasks.lenght > 0 ? (
+                  postDetail.Subtasks.map((subtask, index) => (
+                    <li className="list-group-item" key={index}>
+                      <strong>
+                        {`subtask${index + 1}: `}
+                        {subtask.subTitle}
+                      </strong>
+                      :<br />
+                      <em>description:</em>
+                      <br />
+                      {subtask.description}
+                    </li>
+                  ))
+                ) : (
+                  <li>No Subtask</li>
+                )}
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Event Status: </th>
+            <td>
+              {postDetail.Complete ? (
+                <span>Completed</span>
+              ) : (
+                <span>Not Completed</span>
+              )}
+            </td>
+          </tr>
+          {haveTimeDiff && (
+            <tr>
+              <th scope="row">Time Remain:</th>
+              <td><GetTimeDiff dueDate = {new Date(postDetail.dueDate)} setTimeRemain ={setTimeRemain}></GetTimeDiff></td>
+            </tr>
+          )}
+
+
+        </tbody>
+      </table>
+      <div>
+        <Warning timeRemain = {timeRemain} Complete={postDetail.Complete}></Warning>
+      </div>
+    </div>
+  );
+}
